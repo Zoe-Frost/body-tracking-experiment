ec9794276e26734b5ba400ab8e8774be0a9cc1b6
Zoe-Frost <zoe.frost@mds.ac.nz>
2026-03-16 18:12:17 +1300

    change

--- script.js
+++ script.js
@@ -104,7 +104,17 @@ y + (Math.random()*20 -10),
 
 }
 
+ctx.globalAlpha = 0.25
 
+ctx.drawImage(
+silhouetteImg,
+canvas.width/2 -100,
+canvas.height/2 -200,
+200,
+400
+)
+
+ctx.globalAlpha = 1
 
 }
 
